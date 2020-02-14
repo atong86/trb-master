5ac3910569ca665a29231344c2a9a40e409dbddd
atong86 <60783634+atong86@users.noreply.github.com>
2020-02-14 19:32:56 +0800

    Create test.php

---
+++ test.php
@@ -0,0 +1,78 @@
+<?php
+ $connect = mysqli_connect("localhost", "root", "", "trb_db_master");
+ $query ="SELECT * FROM tricycle_operator ORDER BY id_no DESC";
+ $result = mysqli_query($connect, $query);
+ ?>
+ <!DOCTYPE html>
+ <html>
+      <head>
+           <title>Webslesson Tutorial | Datatables Jquery Plugin with Php MySql and Bootstrap</title>
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+           <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+           <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>
+           <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
+      </head>
+      <body>
+           <br /><br />
+           <div class="container" style="width: 80%">
+                <h3 align="center">Datatables Jquery Plugin with Php MySql and Bootstrap</h3>
+                <br />
+                <div class="table-responsive">
+                     <table id="operator_data" class="table table-striped table-bordered">
+                          <thead>
+                               <tr>
+                                    <td>Toda</td>
+                                    <td>Toda No.</td>
+                                    <td>First name</td>
+                                    <td>Surname</td>
+                                    <td>House No.</td>
+                                    <td>Street</td>
+                                    <td>Barangay</td>
+                                    <td>City</td>
+                                    <td>Case No.</td>
+                                    <td>Make</td>
+                                    <td>Motor No.</td>
+                                    <td>Chasis No.</td>
+                                    <td>Plate No.</td>
+                                    <td>Unit</td>
+                                    <td>Date</td>
+                                    <td>Action</td>
+                               </tr>
+                          </thead>
+                          <?php
+                          while($row = mysqli_fetch_array($result))
+                          {
+                               echo '
+                               <tr>
+                                    <td>'.$row["toda"].'</td>
+                                    <td>'.$row["toda_no"].'</td>
+                                    <td>'.$row["first_name"].'</td>
+                                    <td>'.$row["last_name"].'</td>
+                                    <td>'.$row["house_no"].'</td>
+                                    <td>'.$row["street"].'</td>
+                                    <td>'.$row["barangay"].'</td>
+                                    <td>'.$row["city"].'</td>
+                                    <td>'.$row["case_no"].'</td>
+                                    <td>'.$row["make"].'</td>
+                                    <td>'.$row["motor_no"].'</td>
+                                    <td>'.$row["chasis_no"].'</td>
+                                    <td>'.$row["plate_no"].'</td>
+                                    <td>'.$row["unit"].'</td>
+                                    <td>'.$row["date"].'</td>
+                                    <td><a href="generate.php>Generate</a></td>
+
+                               </tr>
+                               ';
+                          }
+                          ?>
+                     </table>
+                </div>
+           </div>
+      </body>
+ </html>
+ <script>
+ $(document).ready(function(){
+      $('#operator_data').DataTable();
+ });
+ </script>
